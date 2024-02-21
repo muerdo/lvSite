851896738832836a79c0b29701aded3846c7c84b
muerdo <d30625657@gmail.com>
2024-02-20 22:42:59 -0300

    sda

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Inicio' },
+    { href: '/', key: 'how_hilink_work', label: 'Quais os destinos?' },
+    { href: '/', key: 'services', label: 'Serviços' },
+    { href: '/', key: 'pricing ', label: 'Preços ' },
+    { href: '/', key: 'contact_us', label: 'Entre em contato' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Informação',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'Tenha todas as informações sobre sua viagem na sua mão, mesmo offline',
+    },
+    {
+      title: 'Escolha seu destino',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "Escolha um de nossos destinos ou sugira um novo para sua viagem",
+    },
+    {
+      title: 'Tecnologia',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Nossa tecnologia irá ajudar a encontrar o melhor caminho para o seu destino',
+    },
+    {
+      title: 'Novos pacotes promocionais para inscritos',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'Cadastre-se e receba promoções exclusivas',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Conheça mais',
+      links: [
+        'Quem somos',
+        'Galeria de Fotos',
+        'Destinos',
+        'Trabalhe conosco',
+        'Politica de Privacidade',
+        'Contate-nos',
+      ],
+    },
+    {
+      title: 'Nossas Redes Sociais',
+      links: ['Instagram', 'Facebook', 'Whatsapp'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contato',
+    links: [
+      { label: 'Administração tel', value: '99999999' },
+      { label: 'Administração Email', value: 'teste@teste.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
